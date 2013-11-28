b9a6750cef2f1efcd18c9878154f703283cea69e
Ankit Gupta <aigupta@linkedin.com>
2013-11-28 03:14:59 -0800

    Removing debug logging

--- maven-hawtjni-plugin/src/main/java/org/fusesource/hawtjni/maven/GenerateMojo.java
+++ maven-hawtjni-plugin/src/main/java/org/fusesource/hawtjni/maven/GenerateMojo.java
@@ -187,7 +187,7 @@ public class GenerateMojo extends AbstractMojo {
             public void setTotals(int total) {
             }
             public void setMessage(String message) {
-                getLog().info(message + "Ankit");
+                getLog().info(message);
             }
         });
         try {
